966c283642231be143fca8e406137a30b40c26bb
Hemanjali Paiboina <hemanjalipaiboina0718@gmail.com>
2026-05-12 21:24:58 +0530

    added backend route

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,9 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/cart', require('./routes/cart'));
 app.use('/api/orders', require('./routes/orders'));
 
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
